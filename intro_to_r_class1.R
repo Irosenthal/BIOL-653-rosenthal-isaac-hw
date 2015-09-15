e398f4bb45780400d110435223daedd6298aa16c
Irosenthal <irosenthal121@gmail.com>
2015-09-15 09:35:57 -0400

    starting a collection of introductory commands

---
+++ intro_to_r_class1.R
@@ -0,0 +1,33 @@
+# calculator
+2 + 2
+
+# order of operations
+(3 + 3) * 4^4
+
+# scientific notation
+0.0003
+3 * 10^(-4)
+3e-4
+
+# mathematical functions
+log(2) # natural log
+log10(2) #base10 log
+
+exp(0.5) # e^(1/2)
+
+# comparison
+# operators
+
+# equal to
+2==2
+2==3
+2!=2
+2!=3
+
+# greater/less than
+2 < 3
+2 > 3
+2 < 2
+2 <= 2
+2 <= 3
+
