57c5e1faa3f1d864cf87af5b9cb172e663029d93
Irosenthal <irosenthal121@gmail.com>
2015-09-15 10:33:28 -0400

    typo fixes and better flow

--- intro_to_r_class1.R
+++ intro_to_r_class1.R
@@ -31,3 +31,9 @@ exp(0.5) # e^(1/2)
 2 <= 2
 2 <= 3
 
+# syntax
+# functions do something
+# functions take arguemnts
+# function(arg1, arg2, arg3)
+mean(x, trim = 0, na.rm = FALSE)  # x is list of values, trim trims outliers, 0 is default,don't NEED to write it but helps remember, na.rm means do you want me to remove NAs (no data) before calculating mean?
+
